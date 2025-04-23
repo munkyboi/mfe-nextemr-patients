538a45df57de6ff266af1e1e3e6bc9a48a4a475a
munky <munky.work@yahoo.com.sg>
2025-04-23 08:22:03 +0800

    fix export IValidateStatusBody

--- src/lib/api/api.constants.ts
+++ src/lib/api/api.constants.ts
@@ -1,7 +1,7 @@
 export const FABIRCATE_TOKEN = process.env.NEXT_PUBLIC_FABRICATE_TOKEN || '';
 export const QUEUE_POLLING = process.env.NEXT_PUBLIC_QUEUE_POLLING || 3600000;
 
-interface IValidateStatusBody {
+export interface IValidateStatusBody {
   status?: string;
   message?: string;
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,6 +3,7 @@ import { IPhysician } from '@/context/physicians.context';
 import { IQueue, IQueueStatus, QueueFilterType } from '@/context/queue.context';
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import { IValidateStatusBody } from './api/api.constants';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
